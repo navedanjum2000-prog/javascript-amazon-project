5112c58e11a40a8360d6337cbd4e025032b5e2f3
MausTHEMaus <naved.anjum2000@gmail.com>
2026-08-18 02:05:46 +0530

    Add "Added" in add to cart

--- scripts/amazon.js
+++ scripts/amazon.js
@@ -41,7 +41,7 @@ products.forEach((product) => {
 
           <div class="product-spacer"></div>
 
-          <div class="added-to-cart">
+          <div class="added-to-cart js-added-to-cart-${product.id}">
             <img src="images/icons/checkmark.png">
             Added
           </div>
@@ -80,8 +80,8 @@ document.querySelectorAll('.js-add-to-cart').forEach((button) => {
             matchingItem.quantity += quantity;
         } else {
             cart.push({
-                productId: productId,
-                quantity: quantity
+                productId,
+                quantity // shorthand for quatity: quatity and productId: productId
             });
         }
         
@@ -93,9 +93,14 @@ document.querySelectorAll('.js-add-to-cart').forEach((button) => {
 
         document.querySelector('.js-cart-quantity').innerHTML = cartQuantity;
 
-        console.log(cartQuantity);
-        console.log(cart);
 
+        const addedMessage = document.querySelector(`.js-added-to-cart-${productId}`);
+        addedMessage.classList.add('added-to-cart-visible')
+        setTimeout(() => {
+              addedMessage.classList.remove('added-to-cart-visible')
+        }, 1500);
+        
+        
         
     });
 });
