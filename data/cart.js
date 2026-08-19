cc0d1e8fc6b501036455a6be6a40aa88348c5c46
MausTHEMaus <naved.anjum2000@gmail.com>
2026-08-19 13:48:23 +0530

    Generate HTML for the order summary

--- data/cart.js
+++ data/cart.js
@@ -1,7 +1,15 @@
-export const cart = [];
+import { products } from "./products.js";
+
+export const cart = [{
+    productId: 'e43638ce-6aa0-4b85-b27f-e1d07eb678c6',
+    quantity: 2
+}, {
+    productId: '15b6fc6f-327a-4ec4-896f-486349e85a3d',
+    quantity: 1
+}];
 
 
-function addToCart(productId, quantity) {
+export function addToCart(productId, quantity) {
 // checking if the products already exists or not, then we will increase the quantity
   let matchingItem; // we made this var bcz of scope problems (undefined)
 
