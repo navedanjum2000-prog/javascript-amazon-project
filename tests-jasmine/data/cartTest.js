c0a41c3e357cefe5ff9bde37b66b887af24a14a3
MausTHEMaus <naved.anjum2000@gmail.com>
2026-08-26 15:15:52 +0530

    Create automated test for orderSummary WHICH DONT WORK!!!

--- tests-jasmine/data/cartTest.js
+++ tests-jasmine/data/cartTest.js
@@ -11,7 +11,6 @@ describe('test suite: addToCart', () => {
                 deliveryOptionId: '1'
             }]);
         });
-        console.log(localStorage.getItem('cart'));
         loadFromStorage();
 
         addToCart('e43638ce-6aa0-4b85-b27f-e1d07eb678c6');
