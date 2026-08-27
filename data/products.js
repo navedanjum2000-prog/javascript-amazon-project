17fd17fe63e8e0ac0e6434209b8f50714ba8e9b6
MausTHEMaus <naved.anjum2000@gmail.com>
2026-08-28 01:35:40 +0530

    Convt loadProducts from XHR to fetch

--- data/products.js
+++ data/products.js
@@ -97,6 +97,30 @@ obj3.method();
 
 export let products = [];
 
+
+export function loadProductsFetch() {
+  const promise = fetch('https://supersimplebackend.dev/products').then((response) => {            // fetch does GET request by default
+    return response.json();
+  }).then((productsData) => {
+    products = productsData.map((productDetails) => {
+      if (productDetails.type === 'clothing') {
+        return new Clothing(productDetails);
+      }
+      return new Product(productDetails);
+    });
+
+    console.log('load products!');
+
+    return promise;
+  }); 
+};
+/*
+loadProductsFetch().then(() => {
+  console.log('next step');
+});
+*/
+
+
 export function loadProducts(fun) {
   const xhr = new XMLHttpRequest();
 
@@ -110,7 +134,7 @@ export function loadProducts(fun) {
 
     console.log('load products!');
 
-    fun();
+    return promise;
   });
 
   xhr.open('GET', 'https://supersimplebackend.dev/products');
--- scripts/checkout.js
+++ scripts/checkout.js
@@ -2,19 +2,14 @@ import { renderOrderSummary } from "./checkout/orderSummary.js";
 import { renderPaymentSummary } from "./checkout/paymentSummary.js";
 // import '../data/cart-class.js';
 // import '../data/backend-practice.js';
-import { loadProducts } from "../data/products.js";
+import { loadProducts, loadProductsFetch } from "../data/products.js";
 import { loadCart } from "../data/cart.js";
 
 import { renderCheckoutHeader } from "./checkout/checkoutHeader.js"; // // HW-15
 
 
 Promise.all([    // PROMISE.ALL RUNS MULTIPLE PROMISES !!!!!!!!!!!!!!!!! 
-    new Promise((resolve) => {// promised are desined to take a function only! IT WILL RUN FIRST BEFORE ANYTHING, RESOLVE LETS US CONTROL WHEN TO GOTO THE NXT STEP
-        loadProducts(() => {
-            resolve('1st_value');      // first we wait for the loadProducts to finish then resolve will let us go to the next step
-        });                            // THE RESOLVE CAN HAVE VALUES WHICH IS SAVED IN A PARAMETER IN THE 'then'
-
-    }),
+    loadProductsFetch(),
     new Promise((resolve) => {
         loadCart(() => {
             resolve('2ND_VALUE');
